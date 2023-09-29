a73aa5f45c0257973c7c48db5d22fc4b0bf02092
Dauchart <xdauch@gmail.com>
2023-09-29 15:00:02 +0200

    CreatNewUser

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
-    #[Route('/hello', name: 'app_hello')]
+    #[Route('/hellos', name: 'app_hellos')]
     public function index(): Response
     {
         return $this->render('hello/index.html.twig', [
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,19 +2,46 @@
 
 namespace App\Controller;
 
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
+    #[Route('/users', name: 'app_users')]
     public function index(
         
+       UserRepository $userRepository 
     ): Response
     {
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'users' => $userRepository->findAll()
+        ]);
+    }
+    #[Route('/users/new', name: 'app_users_new')]
+    public function add(
+       Request $request, 
+       EntityManagerInterface $entityManagerInterface,
+       UserRepository $userRepository 
+    ): Response
+    {
+        $form = $this->createForm(UserType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $user = $form->getData();
+            $entityManagerInterface->persist(($user));
+            $entityManagerInterface->flush();
+            $this->addFlash("success", "Vous avez bien créé votre compte client");
+            return $this->redirectToRoute("app_users");
+
+        }
+        return $this->render('user/new.html.twig', [
+            "form" => $form,
         ]);
     }
 }
